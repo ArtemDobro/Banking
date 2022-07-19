70e9912ad44b92e8957cd51599405d82588595dd
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-07-19 17:59:29 +0300

    remove unused imports

--- src/main/java/com/andersenlab/rmtbanking/depositservice/mapper/DebitCardsMapper.java
+++ src/main/java/com/andersenlab/rmtbanking/depositservice/mapper/DebitCardsMapper.java
@@ -2,14 +2,11 @@ package com.andersenlab.rmtbanking.depositservice.mapper;
 
 import com.andersenlab.rmtbanking.depositservice.dto.DebitCardsDto;
 import com.andersenlab.rmtbanking.depositservice.dto.DebitCardsInfoDto;
-import com.andersenlab.rmtbanking.depositservice.entity.Account;
 import com.andersenlab.rmtbanking.depositservice.entity.Card;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 import java.util.List;
-import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface DebitCardsMapper {
