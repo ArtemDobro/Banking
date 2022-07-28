a42058f816bd8ecff77dbcb1dcf5025583995ceb
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-07-28 18:06:44 +0300

    implement JavaDoc

--- src/main/java/com/andersenlab/rmtbanking/depositservice/service/DepositService.java
+++ src/main/java/com/andersenlab/rmtbanking/depositservice/service/DepositService.java
@@ -3,5 +3,13 @@ package com.andersenlab.rmtbanking.depositservice.service;
 import com.andersenlab.rmtbanking.depositservice.dto.DetailedDepositDto;
 
 public interface DepositService {
+
+    /**
+     * Return @{@link DetailedDepositDto}.
+     *
+     * @param agreementId @{@link String} agreement id
+     * @param cardId      @{@link String} card id
+     * @return @{@link DetailedDepositDto}.
+     */
     DetailedDepositDto getDetailedDeposit(String agreementId, String cardId);
 }
