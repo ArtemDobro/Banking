1f341c01e094a26ee5a752da19566af6b8a08d31
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-07-28 18:04:06 +0300

    minor changes

--- src/main/java/com/andersenlab/rmtbanking/depositservice/service/DepositService.java
+++ src/main/java/com/andersenlab/rmtbanking/depositservice/service/DepositService.java
@@ -11,7 +11,7 @@ public interface DepositService {
      * Return list of @{@link DepositDto} of account with specific client id.
      *
      * @param clientId @{@link String} client id
-     * @return @{@link List} of @{@link DepositDto}
+     * @return @{@link List} of DepositDto
      */
     List<DepositDto> getAllDeposits(String clientId);
 }
