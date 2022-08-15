f59ddefdd3c44bacfce0bbd5820472296d6f7b0f
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-08-15 11:27:54 +0300

    solve merge conflict

--- src/main/java/com/andersenlab/rmtbanking/depositservice/controller/DepositController.java
+++ src/main/java/com/andersenlab/rmtbanking/depositservice/controller/DepositController.java
@@ -1,16 +1,25 @@
 package com.andersenlab.rmtbanking.depositservice.controller;
 
+import com.andersenlab.rmtbanking.depositservice.dto.DepositDto;
 import com.andersenlab.rmtbanking.depositservice.dto.DetailedDepositDto;
 import com.andersenlab.rmtbanking.depositservice.dto.SwitcherDto;
-import com.andersenlab.rmtbanking.depositservice.service.DepositService;
 import com.andersenlab.rmtbanking.depositservice.service.AgreementService;
+import com.andersenlab.rmtbanking.depositservice.service.DepositService;
 import com.andersenlab.rmtbanking.depositservice.validation.annotation.Uuid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Validated
 @RequestMapping("/auth/deposits")
@@ -21,8 +30,10 @@ public class DepositController {
     private final DepositService depositService;
 
     private final AgreementService service;
+
     /**
      * don't response anything to front, just changing variable in db
+     *
      * @param switcherDto
      */
     @PatchMapping("/auto-renewal")
@@ -31,7 +42,6 @@ public class DepositController {
         service.setAutoRenewal(switcherDto);
     }
 
-    @GetMapping("/{agreementId}")
     @GetMapping("/info/{agreementId}")
     @ResponseStatus(HttpStatus.OK)
     public DetailedDepositDto getDeposit(@Uuid @PathVariable("agreementId") String agreementId, @Uuid @RequestParam String cardId) {
