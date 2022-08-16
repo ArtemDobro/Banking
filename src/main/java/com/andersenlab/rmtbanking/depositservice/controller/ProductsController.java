f98fb46c73d6630f45d3b77bf646c5127cd112c9
aleksandr-pavliuk <a.pavliuk2@gmail.com>
2022-08-16 12:08:50 +0300

    merge dev

--- src/main/java/com/andersenlab/rmtbanking/depositservice/controller/ProductsController.java
+++ src/main/java/com/andersenlab/rmtbanking/depositservice/controller/ProductsController.java
@@ -2,9 +2,11 @@ package com.andersenlab.rmtbanking.depositservice.controller;
 
 import com.andersenlab.rmtbanking.depositservice.entity.Product;
 import com.andersenlab.rmtbanking.depositservice.service.impl.AvailableDepositsServiceImpl;
+import com.andersenlab.rmtbanking.depositservice.validation.annotation.Uuid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,9 @@ import java.util.List;
 public class ProductsController {
 
     private final AvailableDepositsServiceImpl service;
-    @GetMapping
+    @GetMapping("/{clientId}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Product> getAllAvailableProducts() {
+    public List<Product> getAllAvailableProducts(@Uuid @PathVariable("clientId") String clientId) {
         return service.getAllActiveProducts();
     }
 }
